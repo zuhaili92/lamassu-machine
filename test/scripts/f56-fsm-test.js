1b6d4e0656c45212868335c23a3a470a5277b270
Josh Harvey <josh@lamassu.is>
2016-01-08 22:56:09 +0200

    fsm only takes a data byte as input

--- test/scripts/f56-fsm-test.js
+++ test/scripts/f56-fsm-test.js
@@ -1,13 +1,21 @@
-const fsm = require('../../tools/f56-fsm')
-fsm.handle('DLE')
-fsm.handle('ENQ')
-fsm.handle('DLE')
-fsm.handle('STX')
-fsm.handle('Data', 0x00)
-fsm.handle('Data', 0x02)
-fsm.handle('Data', 0x01)
-fsm.handle('Data', 0xa1)
-fsm.handle('DLE')
-fsm.handle('ETX')
-fsm.handle('Data', 0x32)
-fsm.handle('Data', 0xf8)
+const fsm = require('../../lib/f56/f56-fsm')
+
+const STX = 0x02
+const ETX = 0x03
+const ENQ = 0x05
+const ACK = 0x06
+const NAK = 0x15
+const DLE = 0x10
+
+fsm.rx(DLE)
+fsm.rx(ENQ)
+fsm.rx(DLE)
+fsm.rx(STX)
+fsm.rx(0x00)
+fsm.rx(0x02)
+fsm.rx(0x01)
+fsm.rx(0xa1)
+fsm.rx(DLE)
+fsm.rx(ETX)
+fsm.rx(0x32)
+fsm.rx(0xf8)
