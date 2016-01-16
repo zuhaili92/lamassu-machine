72c4708346cac642dc5decf64eb7b84362ab18e3
Josh Harvey <josh@lamassu.is>
2016-01-16 20:42:49 +0000

    tools for iPro recycler testing

---
+++ tools/id003-payout.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const device = process.argv[2]
+const config = {
+  currency: 'EUR',
+  rs232: {device: device}
+}
+
+const id003 = require('../lib/id003/id003').factory(config)
+id003.run(err => {
+  if (err) throw err
+
+  console.log('Bill validator connected.')
+  id003.on('billStay', () => console.log('Bill presented'))
+  id003.on('billPaid', () => console.log('Bill dispensed'))
+
+  id003.setRecycleCurrency(5, 0, 1)
+  delay(200)
+  .then(() => id003.setCurrentCount(10, 1))
+  .then(() => delay(200))
+  .then(() => id003.setRecycleCurrency(20, 0, 2))
+  .then(() => delay(200))
+  .then(() => id003.payout(5, 1))
+})
+
+function delay (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
