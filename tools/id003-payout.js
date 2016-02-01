65a0ed79ada34d702308e0f8551ede458e6fade7
Josh Harvey <josh@lamassu.is>
2016-02-01 17:22:49 +0000

    WIP

--- tools/id003-payout.js
+++ tools/id003-payout.js
@@ -14,17 +14,29 @@ id003.run(err => {
   id003.on('billStay', () => console.log('Bill presented'))
   id003.on('billPaid', () => console.log('Bill dispensed'))
 
-  id003.setRecycleCurrency(5, 0, 1)
+  id003.on('standby', () => {
+    console.log("initialized DEBUG1")
+    id003.disable()
+    delay(1000)
+    .then(() => {
+      console.log("DEBUG2")
+//      id003.setRecycleCurrency(5, 0, 1)
+      id003.payout(5, 1)
+    })	
+  })
 
-  delay200()
-  .then(() => id003.setCurrentCount(1, 1))
-  .then(delay200)
-  .then(() => id003.setRecycleCurrency(20, 0, 2))
-  .then(delay200)
-  .then(() => id003.payout(5, 1))
+//  id003.setRecycleCurrency(5, 0, 1)
+
+//  delay200()
+//  .then(() => id003.setCurrentCount(10, 1))
+//  .then(delay200)
+//  .then(() => id003.setRecycleCurrency(20, 0, 2))
+//  .then(delay200)
 })
 
-const delay200 = () => delay(200)
+function delay200 () {
+  return delay(1000)
+}
 
 function delay (ms) {
   return new Promise(resolve => setTimeout(resolve, ms))
